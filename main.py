98dfe4ea2416f49543fd20a86f52b8ca7b16f7ee
Luxapodular <luca.damasco@gmail.com>
2014-09-08 15:51:22 -0400

    Fixed Some Shtuff

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import time
 import screenconfig
+import streamscreen
 
 def main():
 	#create screen object
-
+        screen = StreamScreen(config.TOP_LEFT, config.BOTTOM_RIGHT)
 	while True:
 		#read pixel
+                rgb = screen.getPixel(50,50)
+                print rgb
+                
 		#send color to led
-		time.sleep(0.2) #sleep 200ms
-		print "d"
+
+                #sleep 200ms
+		time.sleep(0.2) 
