036743600655d6fb16a0979f2a4621bf6a890b20
Luxapodular <luca.damasco@gmail.com>
2014-09-09 14:41:14 -0400

    Started the PixelChooser

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import time
 import screenconfig
 import blinkcontrol
+import datetime
+import pixelchooser
 from streamscreen import StreamScreen
 
 def main():
   #create screen object
   screen = StreamScreen(screenconfig.TOP_LEFT, screenconfig.BOTTOM_RIGHT)
+
+  #choose first pixel
+  currentTime = datetime.datetime.now().time()
+  pixel = pixelchooser.choosePixel((currentTime.minute % 10))
   while True:
+    currentTime = datetime.datetime.now().time()
+    if (currentTime.second == 23):
+      pixel = pixelchooser.choosePixel(currentTime.minute)
+
+    print currentTime
+    print pixel
+      
     #read pixel
-    rgb = screen.getPixel(1700,300)
+    rgb = screen.getPixel(pixel[0],pixel[1])
 
     #send color to led
     blinkcontrol.setColor(rgb);
